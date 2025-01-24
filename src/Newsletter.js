d32647669f40f870fac4a0bc7ce5556f668898e3
TiffanyN22 <tnguyen22@ndsj.org>
2025-01-23 16:44:33 -0800

    Newsletter page goes to mailchimp sign-up

--- src/Newsletter.js
+++ src/Newsletter.js
@@ -1,14 +1,7 @@
 import NavBar from './Components/Navbar'
 import StayUpdated from './Components/StayUpdated'
+import { Link } from "react-router-dom";
 import './Style/Newsletter.css';
-
-const GoogleForm = () => {
-  return (
-    <div className="GoogleForm">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfMojNlZnhz0PJiC3SX7D-k7btD3ow4uSeRFfM6cvPjtxXRag/viewform?embedded=true" width="50%" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-    </div>
-  );
-};
 
 function Newsletter() {
   return (
@@ -17,8 +10,14 @@ function Newsletter() {
       <div className="NewsletterHeading">
         <h1> Join Our Newstter </h1>
       </div>
-      <p className="NewsletterBlurb">Fill out the form below to be added to our newsletter to get updates on future events!</p>
-      <GoogleForm />
+      <div className="NewsletterBlock">
+        <p className="NewsletterBlurb">Enter your SCU email at the mailchimp link below to added to our newsletter to get updates on future events!</p>
+        <button>
+            <Link to="https://gmail.us12.list-manage.com/subscribe?u=2417fc2788acbdf58b4fd02aa&id=868fb760fb&fbclid=PAY2xjawH2jVpleHRuA2FlbQIxMAABptroCfWx-64HFqO9Q0chUqe8X0KQa8eNpBW8hL3i3B1b8N2-xfV4BH86Eg_aem_z7RKlD35fguhZdnNBPlroA" style={{ textDecoration: "none", color: "inherit" }}>
+              Sign Up
+            </Link>
+          </button>
+      </div>
       <StayUpdated></StayUpdated>
     </div>
   );
